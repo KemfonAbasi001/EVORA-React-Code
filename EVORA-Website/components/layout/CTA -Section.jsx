af0b4d7986cc4f30d1aa3a690590c34a7ea62b02
Jonah, KemfonAbasi Mfon <kemfonabasijonah@gmail.com>
2026-08-06 19:34:15 +0100

    New Push

--- EVORA-Website/components/layout/CTA -Section.jsx
+++ EVORA-Website/components/layout/CTA -Section.jsx
@@ -4,7 +4,7 @@ export default function CompareVehicles() {
       <div className="w-[95%] flex flex-col gap-10 lg:flex-row justify-between py-10 rounded-2xl px-6 lg:px-12 lg:items-center" id="lau">
         <div className="flex gap-3 w-full lg:w-[400px] flex-col">
           <h1 className="text-[24px] leading-[32px] lg:text-[32px] lg:leading-[45px] font-semibold text-[#f9fafb]">Ready to Experience Sustainable Mobility?</h1>
-          <p className="text-[14px] lg:text-[15px] text-[#f3f4f6]">Book a test ride and discover the future of driving with EVORA.</p>
+          <p className="lg:text-[14px] text-[16px] lg:text-[15px] text-[#f3f4f6]">Book a test ride and discover the future of driving with EVORA.</p>
           <button className="bg-[#10b981] py-2.75 w-fit px-5 rounded-[6px] text-[15px] lg:text-[14px] text-[#f9fafb] font-medium">
             Book a Test Ride
           </button>
